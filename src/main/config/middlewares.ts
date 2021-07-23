3c23e6d559f016213e2f018f959d65ef28bedeba
Jonatan Colussi <jonatan.colussi@topaziopartner.com.br>
2021-07-23 10:32:16 -0300

    fix: documentation

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express'
 import { bodyParser, cors, contentType, trimParams } from '../middlewares'
 import swaggerUi from 'swagger-ui-express'
-import swaggerDocument from '../../../swagger.json'
+import swaggerDocument from '../../../swagger'
 
 export default (app: Express): void => {
   app.use(bodyParser)
